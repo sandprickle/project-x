2cadb8078806db1409d5891c81deb5439d44f85e
Bryce Miller <sandprickle@gmail.com>
2012-01-16 23:02:27 -0500

    Started on timeline data populator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-document.write("Hello World!");
+var Projx = {
+
+    init: function() {
+        "use strict";
+        var wonkle = document.querySelector('input[type="submit"]');
+        wonkle.addEventListener('submit', Projx.storeData, false);
+    },
+    storeData: function() {
+        "use strict";
+        var evtNameEl = document.querySelector('input[name="evtname"]');
+        var startDateEl = document.querySelector('input[name="startdate"]');
+        var endDateEl = document.querySelector('input[name="enddate"]');
+        
+    }
+};
+
+Projx.init();
