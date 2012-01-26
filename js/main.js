ba437b9213925a14ee110e5d47afd20e2f364ed7
Bryce Miller <sandprickle@gmail.com>
2012-01-26 17:46:32 -0500

    Added license; still working on data storage

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
+// Copyright 2012 Bryce Miller 
+// Licensed under the MIT license (see LICENSE)
+
 var Projx = {
 
     init: function() {
-        "use strict";
+    "use strict";
         var wonkle = document.querySelector('input[type="submit"]');
         wonkle.addEventListener('submit', Projx.storeData, false);
+
+        Projx.evtNameEl = document.querySelector('input[name="evtname"]');
+        Projx.startDateEl = document.querySelector('input[name="startdate"]');
+        Projx.endDateEl = document.querySelector('input[name="enddate"]');
+
+        console.log('Init completed');
     },
-    storeData: function() {
+    storeData: function(event) {
+
         "use strict";
-        var evtNameEl = document.querySelector('input[name="evtname"]');
-        var startDateEl = document.querySelector('input[name="startdate"]');
-        var endDateEl = document.querySelector('input[name="enddate"]');
         
     }
 };
