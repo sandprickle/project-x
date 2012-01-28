2a4625f3ff7dccc8085b3b8ffb317d11d9eee133
Bryce Miller <sandprickle@gmail.com>
2012-01-27 22:31:35 -0500

    Input now stored in Projx.data; next we need to get it from Projx.data to JSON

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-// Copyright 2012 Bryce Miller 
+// Copyright 2012 Bryce Miller
 // Licensed under the MIT license (see LICENSE)
 
 var Projx = {
 
     init: function() {
-    "use strict";
+        "use strict";
         var wonkle = document.querySelector('input[type="submit"]');
-        wonkle.addEventListener('submit', Projx.storeData, false);
+        wonkle.addEventListener('click', Projx.storeData, false);
 
         Projx.evtNameEl = document.querySelector('input[name="evtname"]');
         Projx.startDateEl = document.querySelector('input[name="startdate"]');
         Projx.endDateEl = document.querySelector('input[name="enddate"]');
 
+        Projx.counter = 0;
+
         console.log('Init completed');
     },
+    data: [],
     storeData: function(event) {
-
         "use strict";
-        
+        Projx.data[Projx.counter]= {
+            'evtName': Projx.evtNameEl.value,
+            '(start)Date': Projx.startDateEl.value
+        };
+        if (Projx.endDateEl.value && Projx.endDateEl.value !== '') {
+            Projx.data[Projx.counter].endDate = Projx.endDateEl.value;
+        }
+        Projx.counter += 1;
+        console.log("Data stored");
+        event.preventDefault();
     }
 };
 
