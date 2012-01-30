1c8bcee7f32ab8a082902d5bbdcf300d41ad7e1e
Bryce Miller <sandprickle@gmail.com>
2012-01-30 13:06:55 -0600

    Changed data format and started work on FileSystem storage

--- js/main.js
+++ js/main.js
@@ -4,31 +4,66 @@
 var Projx = {
 
     init: function() {
+        // add event listener to submit button,
+        // store input fields in variables,
+        // counter init
+
         "use strict";
+        
         var wonkle = document.querySelector('input[type="submit"]');
-        wonkle.addEventListener('click', Projx.storeData, false);
-
-        Projx.evtNameEl = document.querySelector('input[name="evtname"]');
-        Projx.startDateEl = document.querySelector('input[name="startdate"]');
-        Projx.endDateEl = document.querySelector('input[name="enddate"]');
-
-        Projx.counter = 0;
+        wonkle.addEventListener('click', Projx.grabData, false);
+        
+        Projx.sDateEl = document.querySelector('input[name="sdate"]');
+        Projx.eDateEl = document.querySelector('input[name="edate"]');
+        Projx.tagNameEl = document.querySelector('input[name="tagname"]');
+        Projx.descEl = document.querySelector('[name="desc"]');
 
         console.log('Init completed');
     },
-    data: [],
-    storeData: function(event) {
+    data: {
+        tags: []
+    },
+    grabData: function(event) {
         "use strict";
-        Projx.data[Projx.counter]= {
-            'evtName': Projx.evtNameEl.value,
-            '(start)Date': Projx.startDateEl.value
+        var c = Projx.data.tags.length;
+        Projx.data.tags[c] = {
+            "name": Projx.tagNameEl.value,
+            "desc": Projx.descEl.value,
+            "sdate": Projx.sDateEl.value,
+            "edate": Projx.eDateEl.value
         };
-        if (Projx.endDateEl.value && Projx.endDateEl.value !== '') {
-            Projx.data[Projx.counter].endDate = Projx.endDateEl.value;
-        }
-        Projx.counter += 1;
+        Projx.c += 1;
+
         console.log("Data stored");
-        event.preventDefault();
+
+        event.preventDefault(); 
+    },
+
+   fileSystemInit: function() {
+        "use strict";
+
+        var reqFS = window.requestFileSystem || window.webkitRequestFileSystem;
+        window.webkitStorageInfo.requestQuota(PERSISTENT, 5*1024*1024, function(b){
+            reqFS(window.PERSISTENT, b, function(fs) {
+                Projx.fs = true;
+                console.log('We got a fs: ' . fs);
+            });
+        }, function(error) {
+            console.log('Oops: ' . error);
+        });
+
+    },
+    storeData: function() {
+        "use strict";
+        if (Projx.fs = true) {
+           // store data
+            console.log('Storing data :) (ok not really)');
+        }
+        else {
+            console.log('We need to initialize the FileSystem.');
+            Projx.fileSystemInit();
+        }
+    
     }
 };
 
